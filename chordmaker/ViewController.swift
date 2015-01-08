7e9ab9a7dcff26455cbf4db776cccbd9a271b6bf
armen <absoboll@gmail.com>
2015-01-07 23:12:38 -0800

    UIPickerViews are working

--- chordmaker/ViewController.swift
+++ chordmaker/ViewController.swift
@@ -11,28 +11,113 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 {
 
-    @IBOutlet weak var firstHarmonicPicker: UIPickerView?
+    @IBOutlet weak var firstHarmonicPicker: UIPickerView!
+    @IBOutlet weak var secondHarmonicPicker: UIPickerView!
+    @IBOutlet weak var thirdHarmonicPicker: UIPickerView!
+    @IBOutlet weak var fourthHarmonicPicker: UIPickerView!
+    @IBOutlet weak var fifthHarmonicPicker: UIPickerView!
     
-    override func viewDidLoad() {
+    @IBOutlet weak var firstHarmonicSlider: UISwitch!
+    @IBOutlet weak var secondHarmonicSlider: UISwitch!
+    @IBOutlet weak var thirdHarmonicSlider: UISwitch!
+    @IBOutlet weak var fourthHarmonicSlider: UISwitch!
+    @IBOutlet weak var fifthHarmonicSlider: UISwitch!
+
+    let firstHarmonicPickerData = ["1st", "Perfect 5th"]
+    let secondHarmonicPickerData = ["2nd", "2nd + M3", "M3", "2nd + m3", "m3", "m7"]
+    let thirdHarmonicPickerData = ["3rd", "3rd + M2", "3rd + M3", "3rd + Tritone", "3rd + 5th Partial", "2/3 Tone 3rd P8 + neutral 6th", "2/3 Tone P8 + P5 + m3", "3rd + M7"]
+    let fourthHarmonicPickerData = ["4th"]
+    let fifthHarmonicPickerData = ["5th"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        firstHarmonicPickerSetup()
+        secondHarmonicPickerSetup()
+        thirdHarmonicPickerSetup()
+        fourthHarmonicPickerSetup()
+        fifthHarmonicPickerSetup()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func firstHarmonicSliderSetup()
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        if pickerView.tag == 1
+        {
+            return firstHarmonicPickerData.count
+        }
+        else if pickerView.tag == 2
+        {
+            return secondHarmonicPickerData.count
+        }
+        else if pickerView.tag == 3
+        {
+            return thirdHarmonicPickerData.count
+        }
+        else if pickerView.tag == 4
+        {
+            return fourthHarmonicPickerData.count
+        }
+        else if pickerView.tag == 5
+        {
+            return fifthHarmonicPickerData.count
+        }
+        else
+        {
+            return 1
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String
+    {
+        if pickerView.tag == 1
+        {
+            return firstHarmonicPickerData[row]
+        }
+        else if pickerView.tag == 2
+        {
+            return secondHarmonicPickerData[row]
+        }
+        else if pickerView.tag == 3
+        {
+            return thirdHarmonicPickerData[row]
+        }
+        else if pickerView.tag == 4
+        {
+            return fourthHarmonicPickerData[row]
+        }
+        else if pickerView.tag == 5
+        {
+            return fifthHarmonicPickerData[row]
+        }
+        else
+        {
+            return ""
+        }
+    }
+    
+    func firstHarmonicPickerSetup()
     {
 /*
         octave (P8)	1,200.0	0.0
         just perfect fifth	P8 + just perfect fifth (P5)	1,902.0	702.0
 */
-
-        let pickerData = ["Octave","Perfect 5th", "OFF"]
+        firstHarmonicPicker.delegate = self
+        firstHarmonicPicker.tag = 1
     }
-    func secondHarmonicSliderSetup()
+    
+    func secondHarmonicPickerSetup()
     {
 /*
 	second octave	2P8	2,400.0	0.0
@@ -40,10 +125,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 	just minor third	2P8 + P5	3,102.0	702.0
 	septimal minor third	2P8 + septimal minor seventh (m7)	3,368.8	968.8
 */
-
-        let pickerData = ["2nd","2nd + M3","M3","2nd + m3","m3", "m7"]
-
-        
+        secondHarmonicPicker.delegate = self
+        secondHarmonicPicker.tag = 2
     }
     
     func thirdHarmonicPickerSetup()
@@ -58,11 +141,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         2/3-tone	3P8 + P5 + septimal minor third (m3)	4,568.8	968.8
         septimal (or major) diatonic semitone	3P8 + just major seventh (M7)	4,688.3	1,088.3
 */
+        thirdHarmonicPicker.delegate = self
+        thirdHarmonicPicker.tag = 3
     }
     
     func fourthHarmonicPickerSetup()
     {
-        
+        //just (or minor) diatonic semitone	4P8	4,800.0	0.0
+        fourthHarmonicPicker.delegate = self
+        fourthHarmonicPicker.tag = 4
+    }
+    
+    func fifthHarmonicPickerSetup()
+    {
+        fifthHarmonicPicker.delegate = self
+        fifthHarmonicPicker.tag = 5
     }
     
     func sliderControls()
